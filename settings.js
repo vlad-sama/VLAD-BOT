695696452d8cf352ecf644a360656218b51b3915
vladimyr <113305717+vlad-sama@users.noreply.github.com>
2025-05-22 13:48:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'vlad pack',
+  author: '‎vlad',
+  botName: "vlad bot",
+  botOwner: 'VLAD', // Your name
+  ownerNumber: '237657312887', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
